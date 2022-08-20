f8973f07903dd315a0e6b34414386041eee80347
GlenioFaria <glenio.faria@gmail.com>
2022-08-20 17:45:43 -0400

    Lambda

--- m3-lambda/src/main/java/com/cydeo/AppleTest.java
+++ m3-lambda/src/main/java/com/cydeo/AppleTest.java
@@ -9,20 +9,26 @@ public class AppleTest {
 
         List<Apple> inventory = new ArrayList<Apple>();
 
-        inventory.add(new Apple(300,Color.GREEN));
-        inventory.add(new Apple(100,Color.RED));
-        inventory.add(new Apple(200,Color.GREEN));
-        inventory.add(new Apple(500,Color.RED));
+        inventory.add(new Apple(300, Color.GREEN));
+        inventory.add(new Apple(100, Color.RED));
+        inventory.add(new Apple(200, Color.GREEN));
+        inventory.add(new Apple(500, Color.RED));
 
+        AppleHeavyPredicate abc =  new AppleHeavyPredicate();
+        List<Apple> heavy = filterApples(inventory,abc);
+        System.out.println(heavy);
+
+        List<Apple> greenApple = filterApples(inventory, new AppleGreenColorPredicate());
+        System.out.println(greenApple);
 
 
     }
 
-    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate){
+    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate) {
 
         List<Apple> result = new ArrayList<>();
-        for (Apple apple : inventory){
-            if (applePredicate.test(apple)){
+        for (Apple apple : inventory) {
+            if (applePredicate.test(apple)) {
                 result.add(apple);
             }
         }
