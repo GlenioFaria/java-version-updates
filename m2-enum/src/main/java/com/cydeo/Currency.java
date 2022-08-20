5de8e39dce12e29ff8fccc43ca279321090a309a
GlenioFaria <glenio.faria@gmail.com>
2022-08-20 15:37:03 -0400

    Enum

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -2,7 +2,15 @@ package com.cydeo;
 
 public enum Currency { // Final Class
 
-    PENNY, NICKLE, DIME , QUARTER // static final by default
+    PENNY(1), NICKLE(5), DIME (10), QUARTER(25) ;// static final by default ;
 
+    private int value;
 
+    Currency(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
--- m2-enum/src/main/java/com/cydeo/Operation.java
+++ m2-enum/src/main/java/com/cydeo/Operation.java
@@ -2,6 +2,9 @@ package com.cydeo;
 
 public enum Operation {
 
-    PLUS, MINUS, MULTIPLY, DIVIDE
+    PLUS, MINUS, MULTIPLY, DIVIDE; // each constant is a object (new) of Operation
 
+    private Operation() { // Constructor private by default can not change
+        System.out.println("Constructor");
+    }
 }
