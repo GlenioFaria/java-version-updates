0e5ae65d6f2f71ed879b7d6f0c97cede47c2a918
GlenioFaria <glenio.faria@gmail.com>
2022-08-21 13:22:17 -0400

    Comparator Interface

---
+++ m5-comparator_interface/src/main/java/com/cydeo/AppleTest.java
@@ -0,0 +1,18 @@
+package com.cydeo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleTest {
+
+    public static void main(String[] args) {
+
+        List<Apple> inventory = new ArrayList<Apple>();
+
+        inventory.add(new Apple(300, Color.GREEN));
+        inventory.add(new Apple(100, Color.RED));
+        inventory.add(new Apple(200, Color.GREEN));
+        inventory.add(new Apple(500, Color.RED));
+
+    }
+}
--- m5-comparator_interface/src/main/java/com/cydeo/MAin.java
+++ m5-comparator_interface/src/main/java/com/cydeo/MAin.java
@@ -12,13 +12,23 @@ public class MAin {
         List<Integer> list = Arrays.asList(10, 40, 0, 20, 30);
         System.out.println(list);
 
+        //Ascending
         Collections.sort(list);
         System.out.println(list);
 
+        //Descending
         Collections.sort(list, new MyComparator());
         System.out.println(list);
 
         Collections.sort(list, ((o1,o2)-> (o1 > o2) ? -1 : (o1<o2) ? 1 : 0));
+
+        //Ascending order
+        list.sort((o1,o2)->o1.compareTo(o2));
+        System.out.println(list);
+
+        //Descending order
+        list.sort((o2,o1)->o1.compareTo(o2));
+        System.out.println(list);
 
 
 
