5b9b9b3a64bf513851e07607ee744e92a1e85fcd
GlenioFaria <glenio.faria@gmail.com>
2022-08-21 19:06:33 -0400

    FlatMAp

--- m6-stream/src/main/java/com/cydeo/task/EmployeeTest.java
+++ m6-stream/src/main/java/com/cydeo/task/EmployeeTest.java
@@ -1,9 +1,25 @@
 package com.cydeo.task;
 
+import java.util.List;
+
 public class EmployeeTest {
 
     public static void main(String[] args) {
 
+        System.out.println("Print All the email");
+        EmployeeData.readAll()
+                .map(Employee::getEmpEmail)
+                .forEach(System.out::println);
 
+        System.out.println("Print all Phone Numbers");
+        EmployeeData.readAll()
+                .flatMap(employee -> employee.getEmpPhoneNumbers().stream())
+                .forEach(System.out::println);
+
+        System.out.println("Print all Phone Numbers with double colon");
+        EmployeeData.readAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .flatMap(List:: stream)
+                .forEach(System.out::println);
     }
 }
