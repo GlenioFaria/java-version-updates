00e0f95aaa2132949ff426c6bc0c4a802a74948b
GlenioFaria <glenio.faria@gmail.com>
2022-08-21 21:15:04 -0400

    Reduce

--- m6-stream/src/main/java/com/cydeo/Reducing.java
+++ m6-stream/src/main/java/com/cydeo/Reducing.java
@@ -40,6 +40,13 @@ public class Reducing {
         System.out.println("Max: "+max.get());
         System.out.println("Sum: "+sum.get());
 
+        System.out.println("COUNT");
+        long dishCount = DishData.getAll().stream().count();
+        System.out.println(dishCount);
+
+
+
+
 
 
 
