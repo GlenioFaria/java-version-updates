e408ba3f21da927cfa263f105433561e3006a77b
GlenioFaria <glenio.faria@gmail.com>
2022-08-22 14:29:22 -0400

    Java 9

--- m8-new-version-updates/src/main/java/com/cydeo/java9/FactorymethodDemo.java
+++ m8-new-version-updates/src/main/java/com/cydeo/java9/FactorymethodDemo.java
@@ -7,24 +7,23 @@ public class FactorymethodDemo {
     public static void main(String[] args) {
 
         //Creating unmodifiable LIst before JAva9
-        List<String> courses = Collections.unmodifiableList(Arrays.asList("Java","Spring","Agile"));
-       // courses.add("TS");
+        List<String> courses = Collections.unmodifiableList(Arrays.asList("Java", "Spring", "Agile"));
+        // courses.add("TS");
         System.out.println(courses);
 
         //Creating unmodifiable LIst after JAva9
-        List<String> myCourses = List.of("Java","Spring","Agile");
+        List<String> myCourses = List.of("Java", "Spring", "Agile");
 
         //Creating unmodifiable Set after JAva9
-        Set<String> myProduct = Set.of("Milk","Bread","Butter");
+        Set<String> myProduct = Set.of("Milk", "Bread", "Butter");
 
         //Creating unmodifiable Map after JAva9
         Map<String, Integer> myCart = Map.ofEntries(
-                Map.entry("Samsung TV",1),
-                Map.entry("PSP",1),
-                Map.entry("Chair",5)
+                Map.entry("Samsung TV", 1),
+                Map.entry("PSP", 1),
+                Map.entry("Chair", 5)
 
         );
-
 
 
     }
